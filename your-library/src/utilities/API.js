35c9acba7510c21cd6af14d0eeba54d613e5b062
Mlipphardt <matthewlipphardt@gmail.com>
2020-04-29 17:02:21 -0400

    Google API GET added

---
+++ your-library/src/utilities/API.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import apikey from "./config";
+
+export default {
+  // Gets all books
+  googleBooks: function (query) {
+    return axios.get("https://www.googleapis.com/books/v1/volumes?q=" + query);
+  },
+  // Gets the book with the given id
+  getSaved: function (id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteSaved: function (id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function (bookData) {
+    return axios.post("/api/books", bookData);
+  },
+};
