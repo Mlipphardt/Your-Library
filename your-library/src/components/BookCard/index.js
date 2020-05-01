8d79e06fbd0c46cc5e5aa62d2010fb017fe43de7
Mlipphardt <matthewlipphardt@gmail.com>
2020-05-01 11:16:57 -0400

    BookCard component altered, Navbar and Search styling altered

---
+++ your-library/src/components/BookCard/index.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+function BookCard(props) {
+  return (
+    <div className="book-div card my-4">
+      <div className="card-header bg-primary">
+        <div className="row">
+          <div className="col-md-3">
+            <h3>{props.title}</h3>
+          </div>
+          <div className="col-md-7"></div>
+          <div className="col-md-2">
+            <button className="btn bg-success view-button">View</button>
+            <button className="btn bg-success save-button">Save</button>
+          </div>
+        </div>
+      </div>
+      <div className="card-body">
+        <div className="row">
+          <div className="col-md-3">
+            <img
+              src={props.image}
+              alt={props.title}
+              height="200px"
+              width="200px"
+            />
+          </div>
+          <div className="col-md-9">{props.description}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default BookCard;
+
+/*
+<div class="article-div card my-4">
+            <div class="card-header bg-primary">
+                <a href={{this.link}}>{{this.title}}</a>
+            </div>
+            <div class="card-body">
+                <p>{{this.description}}</p>
+            </div>
+            <div class="card-footer bg-info text-right">
+                <button class="btn bg-success notes-button" data-toggle="modal" data-target="#note-modal"
+                    data-id={{this._id}}>Notes</button>
+                {{#if favorite}}
+                <button class="btn bg-warning favorite-button" data-favorited={{this.favorite}} data-id={{this._id}}>In
+                    Favorites</button> {{else}} <button class="btn bg-success favorite-button"
+                    data-favorited={{this.favorite}} data-id={{this._id}}>Favorite</button>
+                {{/if}}
+            </div>
+        </div>
+*/
--- your-library/src/pages/Search.js
+++ your-library/src/pages/Search.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import BookCard from "../components/BookCard";
 import API from "../utilities/API";
 
 class Search extends Component {
@@ -16,17 +17,37 @@ class Search extends Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <form id="query-form">
-            <label>Search Books, Authors, Summaries...</label>
-            <input type="text" name="query" id="query-input" />
-          </form>
-          <button type="submit" form="query-form">
-            Submit
-          </button>
+        <div className="row mt-5 mb-3 text-center">
+          <div className="col-md-2"></div>
+          <div className="col-md-8">
+            <form id="query-form">
+              <div>
+                <label>Search Books, Authors, Summaries...</label>
+              </div>
+              <input
+                type="text"
+                name="query"
+                id="query-input"
+                className="my-3"
+                size="50"
+              />
+            </form>
+            <button type="submit" className="btn bg-primary" form="query-form">
+              Submit
+            </button>
+          </div>
+          <div className="col-md-2"></div>
         </div>
         <div className="row">
-          <p>Hello World</p>
+          <div className="col-md-1"></div>
+          <div className="col-md-10">
+            <BookCard
+              title="test"
+              description="testy mcTestFace"
+              image="you what mate?"
+            />
+          </div>
+          <div className="col-md-1"></div>
         </div>
       </div>
     );
