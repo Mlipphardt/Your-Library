4c0bc9a1d89451d2bfcc3c841a5851590791af99
Mlipphardt <matthewlipphardt@gmail.com>
2020-05-01 20:40:42 -0400

    Navbar links added

--- your-library/src/components/Navbar/index.js
+++ your-library/src/components/Navbar/index.js
@@ -7,6 +7,18 @@ function Nav() {
       <a className="navbar-brand" href="/">
         Your Library
       </a>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="/" className="nav-link">
+            Search
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="/books" className="nav-link">
+            Saved Books
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
